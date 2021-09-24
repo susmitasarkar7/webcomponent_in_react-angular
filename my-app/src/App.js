6bd4ba1cb59010b7810ad790b2ca9657f38c2d75
Susmita Sarkar <sarkarsusmita253@gmail.com>
2021-09-24 21:07:36 +0530

    Initial Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+    this.component.addEventListener('feedbackSubmit', this.onFeedbackSubmit);
+  }
+
+  componentWillUnmount() {
+    this.component.removeEventListener('feedbackSubmit', this.onFeedbackSubmit);
+  }
+
+  onFeedbackSubmit(event) {
+    console.log('Got', event.detail);
+  }
+
+  handleRef = component => {
+    this.component = component;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Angular in React</p>
+        </header>
+  
+        <app-form name="Susmita" ref={this.handleRef} ></app-form>
+  
+      </div>
+    );
+  }
+}
+
+export default App;
